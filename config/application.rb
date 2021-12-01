be84d6806a5d40e0b5f12200ef7db6452ab44473
Taiki NAYA <taiki780528@gmail.com>
2021-12-01 17:16:07 +0900

    generateコマンドでの自動生成内容を設定

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module App01
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
