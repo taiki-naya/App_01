de14b36cb518213b237059ac5f0528a00f55cecd
Taiki NAYA <taiki780528@gmail.com>
2021-12-15 22:50:40 +0900

    Profileモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_121848) do
+ActiveRecord::Schema.define(version: 2021_12_15_134905) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "name", null: false
@@ -75,6 +75,16 @@ ActiveRecord::Schema.define(version: 2021_12_15_121848) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.string "jid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "shoppers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -135,5 +145,6 @@ ActiveRecord::Schema.define(version: 2021_12_15_121848) do
   add_foreign_key "items", "stores"
   add_foreign_key "kits", "teams"
   add_foreign_key "labelling_of_posts", "posts"
+  add_foreign_key "profiles", "users"
   add_foreign_key "teams", "leagues"
 end
