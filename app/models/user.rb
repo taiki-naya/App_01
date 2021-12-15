38c1b79a2527773c4665d4310d3096fb9ed8a828
Taiki NAYA <taiki780528@gmail.com>
2021-12-15 23:58:20 +0900

    登録フォームにプロフィール入力機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_one :profile, dependent: :destroy, class_name: Profile
+         has_one :profile, dependent: :destroy
+         accepts_nested_attributes_for :profile
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :shoppers
-  devise_for :users
+  devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+}
   root to: 'leagues#index'
   resources :leagues do
     resources :teams do
