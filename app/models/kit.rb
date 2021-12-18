38bcaab2afd8331aa414587c3ef01fef80e96c02
Taiki NAYA <taiki780528@gmail.com>
2021-12-19 01:57:03 +0900

    バリデーション追加

--- app/models/kit.rb
+++ app/models/kit.rb
@@ -3,4 +3,6 @@ class Kit < ApplicationRecord
   has_one_attached :image
   has_many :labelling_of_posts, as: :labelable
   has_many :labelling_of_items, as: :labelable
+
+  validates :series, presence: true
 end
